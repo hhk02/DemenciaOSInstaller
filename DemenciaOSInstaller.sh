e50d32e763a5ff8a82f41df9bbd2d7641171bd6c
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-02-07 13:38:24 -0600

    Add files via upload

--- DemenciaOSInstaller.sh
+++ DemenciaOSInstaller.sh
@@ -70,7 +70,7 @@ GetNala() {
 
 # Instalación de nucleo / kernel para el destino (Instalar kernel para usar el sistema)
 InstallKernel() {
-	arch-chroot /mnt /bin/bash -c 'mount ', $efipart , '/boot'
+	cp -rv /boot/* /mnt/boot
 	arch-chroot /mnt /bin/bash -c 'apt install wget -y'
 	echo "What kernel you do want (generic/xanmod)? WARNING: The XanMod kernel or others kernels maybe causes errors to install NVIDIA video cards"
 	read choosekernel
