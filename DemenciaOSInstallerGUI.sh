8ea3270e38c49dfe68c57c3059373c75f4a92292
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-02-08 20:16:46 -0600

    Add files via upload

--- DemenciaOSInstallerGUI.sh
+++ DemenciaOSInstallerGUI.sh
@@ -23,7 +23,7 @@ InstallWezTerm() {
 	arch-chroot /mnt /bin/bash -c 'apt install -y ./wezterm-20221119-145034-49b9839f.Debian11.deb'
 	echo "WezTerm Installed"
 	) |
-	zenity --progress \
+		zenity --progress \
   		--title="Installing WezTerm" \
   		--text="Installing" \
   		--percentage=0
@@ -116,12 +116,16 @@ CreateUser() {
 GetNala() {
 	(
 	echo "50"
-	arch-chroot /mnt /bin/bash -c 'curl -O https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb' sleep 1
+	arch-chroot /mnt /bin/bash -c 'curl -O https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb' 
+	sleep 1
 	echo "60"
-	arch-chroot /mnt /bin/bash -c 'curl -O https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb' sleep 1
+	arch-chroot /mnt /bin/bash -c 'curl -O https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb' 
+	sleep 1
 	echo "70"
-	arch-chroot /mnt /bin/bash -c 'apt install ./volian-archive*.deb  -y' sleep 1
-    arch-chroot /mnt /bin/bash -c 'apt update && apt install nala-legacy -y' sleep 1
+	arch-chroot /mnt /bin/bash -c 'apt install ./volian-archive*.deb  -y' 
+	sleep 1
+    arch-chroot /mnt /bin/bash -c 'apt update && apt install nala-legacy -y' 
+    sleep 1
 	echo "100"
 	) |
 	zenity --progress \
