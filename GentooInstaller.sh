84ff3c8db056418aaeb59a60fa9c28c9d633e369
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-02-14 21:15:57 +0100

    Update GentooInstaller.sh

--- GentooInstaller.sh
+++ GentooInstaller.sh
@@ -6,25 +6,10 @@ network_device=""
 disk=""
 efi_partition=""
 root_partition=""
-timezone=""
 selection="1"
 hostname="DemenciaOS"
 if [[ $EUID = 0 ]]; then
 	echo "Welcome to the Gentoo Installer by hhk02 THIS IT'S A EXPERIMENTAL BUILD SO I'AM NOT RESPONSABLE A DATA LOSE!"
-	echo "Changing root password"
-	passwd
-	echo "Showing network devices"
-	ip addr
-	echo "Write your network device for enable: "
-	read network_device
-	if [ -z $network_device ]; then
-		echo "Write your network device for enable: "
-		read network_device
-	else
-		net-setup $network_device
-		dhcpcd $network_device
-	fi
-	
 	echo "Write your disk device ex: /dev/sda"
 	read disk
 	if [ -z $disk ]; then
@@ -53,7 +38,7 @@ if [[ $EUID = 0 ]]; then
 	mount $root_partition /mnt/gentoo
 	cd /mnt/gentoo
 	echo "Installing Gentoo with SystemD PD: He ahi la importancia de SystemD :-)"
-	wget https://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/stage3-amd64-desktop-systemd-20230129T164658Z.tar.xz
+	wget http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/stage3-amd64-desktop-systemd-20230129T164658Z.tar.xz
 	echo "Extracting"
 	tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 	echo "# Configuraciones del compilador a aplicar en cualquier lenguaje\n
@@ -69,68 +54,57 @@ if [[ $EUID = 0 ]]; then
 	echo "Copying default repository configuration!"
 	cp -v /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 	cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
-	mount --types proc /proc /mnt/gentoo/proc
-	mount --rbind /sys /mnt/gentoo/sys
-	mount --make-rslave /mnt/gentoo/sys
-	mount --rbind /dev /mnt/gentoo/dev
-	mount --make-rslave /mnt/gentoo/dev
-	mount --bind /run /mnt/gentoo/run 
-	mount --make-slave /mnt/gentoo/run
 	echo "Changing into target.."
-	source /mnt/gentoo/etc/profile
-	export PS1="(/mnt/gentoo) ${PS1}"
-	mount $efi_partition /boot
-	emerge-webrsync
+	source /etc/profile
+	arch-chroot /mnt/gentoo /bin/bash -c mount $efi_partition /boot
+	arch-chroot /mnt/gentoo /bin/bash -c emerge-webrsync
 	echo "Syncing repos!"
-	emerge --sync
-	emerge --sync --quiet
-	eselect profile list
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --sync
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --sync --quiet
+	arch-chroot /mnt/gentoo /bin/bash -c eselect profile list
 	echo "Write you do want: ex: 2"
 	read selection
-	eselect profile set $selection
-	echo "Write the timezone ex : Europe/Madrid"
-	read timezone
-	ln -sf ../usr/share/zoneinfo/$timezone /etc/localtime
+	arch-chroot /mnt/gentoo /bin/bash -c eselect profile set $selection
+	arch-chroot /mnt/gentoo /bin/bash -c ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 	echo "Done!"
 	echo "es_ES.UTF-8 UTF-8"
 	echo "es_MX.UTF-8 UTF-8"
 	echo "Write in the locale.gen!"
 	
-	nano -w /etc/locale.gen
-	locale-gen
-	env-update && source /etc/profile
-	export PS1="(chroot) ${PS1}"
+	nano -w /mnt/gentoo/etc/locale.gen
+	arch-chroot /mnt/gentoo /bin/bash -c locale-gen
+	arch-chroot /mnt/gentoo /bin/bash -c env-update && source /etc/profile
+	arch-chroot /mnt/gentoo /bin/bash -c export PS1="(chroot) ${PS1}"
 	echo "Installing kernel...."
-	emerge --oneshot sys-kernel/gentoo-kernel-bin
-	emerge --oneshot sys-kernel/linux-headers
-	emerge --oneshot sys-kernel/linux-firmware
-	emerge --oneshot sys-kernel/genkernel
-	emerge --oneshot genfstab
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot sys-kernel/gentoo-kernel-bin
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot sys-kernel/linux-headers
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot sys-kernel/linux-firmware
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot sys-kernel/genkernel
 	genfstab /mnt/gentoo > /mnt/gentoo/etc/fstab
-	genkernel all
-	ls /boot/vmlinu* /boot/initramfs*
+	arch-chroot /mnt/gentoo /bin/bash -c genkernel all
+	ls /mnt/gentoo/boot/vmlinu* /mnt/gentoo/boot/initramfs*
 	echo "Cleaning..."
-	emerge --depclean
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --depclean
 	echo "Set the hostname:" 
 	read hostname
-	hostnamectl hostname $hostname
-	emerge --oneshot net-misc/dhcpcd
-	systemctl enable --now dhcpcd
+	echo $hostname > /mnt/gentoo/etc/hostname
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot net-misc/dhcpcd
+	arch-chroot /mnt/gentoo /bin/bash -c systemctl enable --now dhcpcd
 	echo "Creating hosts"
-	nano -w /etc/hosts
-	emerge --oneshot sys-apps/pcmciautils
-	passwd
-	systemd-firstboot --prompt --setup-machine-id
-	systemctl preset-all
+	touch /mnt/gentoo/etc/hosts
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot sys-apps/pcmciautils
+	arch-chroot /mnt/gentoo /bin/bash -c passwd
+	arch-chroot /mnt/gentoo /bin/bash -c systemd-firstboot --prompt --setup-machine-id
+	arch-chroot /mnt/gentoo /bin/bash -c systemctl preset-all
 	echo "Installing Wireless support"
-	emerge --oneshot net-wireless/iw net-wireless/wpa_supplicant
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot net-wireless/iw net-wireless/wpa_supplicant
 	echo "Installing GRUB"
 	echo 'GRUB_PLATFORMS="efi-64"' >> /mnt/gentoo/etc/portage/make.conf
-	emerge --oneshot --verbose sys-boot/grub
-	emerge --update --newuse --verbose sys-boot/grub
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --oneshot --verbose sys-boot/grub
+	arch-chroot /mnt/gentoo /bin/bash -c emerge --update --newuse --verbose sys-boot/grub
 	echo "Installing bootloader!"
-	grub-install --target=x86_64-efi --efi-directory=/boot
-	grub-mkconfig -o /boot/grub/grub.cfg
+	arch-chroot /mnt/gentoo /bin/bash -c grub-install --target=x86_64-efi --efi-directory=/boot
+	arch-chroot /mnt/gentoo /bin/bash -c grub-mkconfig -o /boot/grub/grub.cfg
 	systemctl reboot	
 else
 	echo "You must run this as root!"
